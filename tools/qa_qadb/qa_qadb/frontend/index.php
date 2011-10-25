47855c1966f238ed7c0225251e58ef95ac97d7fd
Vilem Marsik <vmarsik@suse.cz>
2011-10-25 16:31:15 +0200

    Removed obsolete update of 'tests' from frontend.

--- tools/qa_qadb/qa_qadb/frontend/index.php
+++ tools/qa_qadb/qa_qadb/frontend/index.php
@@ -3,14 +3,10 @@ require_once('qadb.php');
 common_header(array('title'=>'Welcome to the QA database'));
 print "<center><h2>Welcome to the QA database</h2>\n";
 
-$update_link='';
 if ( !isset($_SESSION['user'] ) )
 	printf("you are currently not loged in<br><br>");
 else
-{
 	printf("you are currently loged in as <i><b>%s</b></i><br/><br/>", $_SESSION['user']);
-	$update_link='<div class="screen space"><a href="index.php?update_stats=1">Update testcase stats (takes a long time)</a></div>';
-}
 $last_sub=row_query(array(1), "SELECT submissionID, submission_date FROM submissions ORDER BY submission_date DESC");
 $num_tr=scalar_query("SELECT MAX(resultsID) FROM results");
 $num_prod=scalar_query("SELECT COUNT(distinct productID, releaseID) FROM submissions;");
@@ -30,15 +26,6 @@ $tbl=array(
 	array('DB schema version:',$schema_version),
 );
 print html_table($tbl,array('class'=>'overview','header'=>0,'evenodd'=>false));
-
-if( http('update_stats') )
-{
-	printf("Updating stats ...");
-	update_result(update_test_statistics());
-	printf("Done.");
-}
-else
-	printf($update_link);
 
 print "</center>\n";
 ?>
