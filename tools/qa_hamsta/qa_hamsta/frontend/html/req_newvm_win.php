c0948022dc75ab4e66c270bcfc3996fee8c64e78
Arthur Zhengyuan Guo <aguo@suse.com>
2012-05-22 14:16:37 +0800

    Windows installs in QA Cloud. Will update and optimise source codes soon.

---
+++ tools/qa_hamsta/qa_hamsta/frontend/html/req_newvm_win.php
@@ -0,0 +1,47 @@
+<?php
+/* ****************************************************************************
+  Copyright (c) 2011 Unpublished Work of SUSE. All Rights Reserved.
+  
+  THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+  CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+  RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+  THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+  THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+  TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+  PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+  PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+  AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+  LIABILITY.
+  
+  SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+  AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+  LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+  WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+  ****************************************************************************
+ */
+?>
+
+  <tr>
+    <td width=380>Installation ISO (required): </td>
+    <td>
+      <label for="win_product">Product:</label>
+      <!-- Will update this part later -->
+      <select name="win_product" id="win_products">
+        <option value="/VIRT-ISO/win-2k-sp4-32-auto.iso">win-2k-sp4-32</option>
+        <option value="/VIRT-ISO/win-2k3-sp2-32-auto.iso">win-2k3-sp2-32</option>
+        <option value="/VIRT-ISO/win-2k3-sp2-64-auto.iso">win-2k3-sp2-64</option>
+        <option value="/VIRT-ISO/win-2k8-sp2-32-auto.iso">win-2k8-sp2-32</option>
+        <option value="/VIRT-ISO/win-2k8-sp2-64-auto.iso">win-2k8-sp2-64</option>
+        <option value="/VIRT-ISO/win-2k8r2-fcs-64-auto.iso">win-2k8r2-fcs-64</option>
+        <option value="/VIRT-ISO/win-7-fcs-32-auto.iso">win-7-fcs-32</option>
+        <option value="/VIRT-ISO/win-7-fcs-64-auto.iso">win-7-fcs-64</option>
+        <option value="/VIRT-ISO/win-vista-sp2-32-auto.iso">win-vista-sp2-32</option>
+        <option value="/VIRT-ISO/win-vista-sp2-64-auto.iso">win-vista-sp2-64</option>
+        <option value="/VIRT-ISO/win-xp-sp3-32-auto.iso">win-xp-sp3-32</option>
+      </select>
+      <span id="repo_archs_warning" class="text-red text-small bold">*</span>
+    </td>
+  </tr>
