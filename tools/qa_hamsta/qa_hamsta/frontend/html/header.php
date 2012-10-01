d81406864aae634f742c1d16fd94966b17569b6b
Pavel Kacer <pkacer@suse.com>
2012-10-01 11:23:07 +0200

    Hamsta: Added DOCTYPE (HTML 4.01 Trans.) to header.

--- tools/qa_hamsta/qa_hamsta/frontend/html/header.php
+++ tools/qa_hamsta/qa_hamsta/frontend/html/header.php
@@ -31,6 +31,7 @@
 require_once("include/Util.php");
 
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <title><?php if(!empty($html_title)) echo($html_title." - "); ; ?>HAMSTA</title>
