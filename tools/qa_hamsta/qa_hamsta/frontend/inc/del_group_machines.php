381d73bd4abca801155cc4440a2ed5503d450014
yaojia <jyao@suse.com>
2012-10-23 14:59:39 +0800

    Fate #310587, be add/remove machines from a group

---
+++ tools/qa_hamsta/qa_hamsta/frontend/inc/del_group_machines.php
@@ -0,0 +1,67 @@
+<?php
+/* ****************************************************************************
+  Copyright (c) 2011 Unpublished Work of SUSE. All Rights Reserved.
+  
+  THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+  CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+  RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+  THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+  THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+  TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+  PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+  PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+  AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+  LIABILITY.
+  
+  SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+  AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+  LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+  WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+  ****************************************************************************
+ */
+
+    /**
+     * Logic of the group_del_machines page
+     */
+    if (!defined('HAMSTA_FRONTEND')) {
+        $go = 'del_group_machines';
+        return require("index.php");
+    }
+
+    $name = request_str("group");
+    $group = Group::get_by_name($name);
+    $machines = $group->get_machines();
+    
+    if (request_str("submit")) {
+		$machine_list = request_array("machine_list");
+		if (is_null($machine_list)) {
+			$error = "No groups selected.";
+		} else {
+			$failed = 0;
+			
+			$machine_num = count($machine_list);
+
+			$search = new MachineSearch();
+			$search->filter_in_array($machine_list);
+			$machines = $search->query();
+
+			foreach ($machines as $machine) {
+				if (!$group->del_machine($machine)) {
+					$failed++;
+				}
+			} //end of foreach
+		}
+        if (empty($error)) {
+            if ($failed) {
+                $error = $failed . " machine(s) could not be deleted (possibly were not member?)";
+            }
+            $go = "groups";
+            return require('inc/groups.php');
+        }
+    }
+
+    $html_title = "Remove from group";
+?>
