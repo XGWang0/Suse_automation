69d489ba65a6b703bff14e626d2d1dc5094d0585
Pavel Kacer <pkacer@suse.com>
2012-11-22 17:37:00 +0100

    QADB: DB changes: adding role, modifying tester and connecting them.

---
+++ tools/qa_qadb/qa_qadb/db/patches/201211211519_add_attributes_to_tester.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `tester`
+	MODIFY `tester` varchar(50) NOT NULL COMMENT 'Testers login',
+	ADD COLUMN `ext_ident` varchar(255) COMMENT 'External identifier',
+	ADD COLUMN `name` varchar(100) COMMENT 'Name of the tester',
+	ADD COLUMN `email` varchar(80) COMMENT 'Email contact',
+	ADD COLUMN `password` varchar(60) NOT NULL COMMENT 'Testers password';
