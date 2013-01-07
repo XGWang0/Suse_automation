d89e5a517a4b383ccf9501c81b75f9b4677ad10e
Vilem Marsik <vmarsik@suse.cz>
2013-01-07 15:22:12 +0100

    Fix for QAconf DB schema

--- tools/qa_hamsta/qa_hamsta/db/patches/201301041307_qaconf_sync.sql
+++ tools/qa_hamsta/qa_hamsta/db/patches/201301041307_qaconf_sync.sql
@@ -1,3 +1,6 @@
 ALTER TABLE qaconf ADD COLUMN sync_url VARCHAR(2048) NULL;
+UPDATE machine SET qaconf_id=NULL;
+UPDATE `group` SET qaconf_id=NULL;
+DELETE FROM qaconf_row;
 DELETE FROM qaconf;
 INSERT INTO qaconf(qaconf_id,`desc`,sync_url) VALUES (1,'global configuration','http://qadb.suse.de/global.conf'),(2,'country configuration','http://qadb.suse.de/blank.txt'),(3,'site configuration','http://qadb.suse.de/blank.txt'),(4,'master configuration',NULL);
