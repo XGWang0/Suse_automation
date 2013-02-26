daa668dc3657e46bd26fd6130c51421e7f254ca9
Vilem Marsik <vmarsik@suse.cz>
2013-02-26 12:28:27 +0100

    Added new permissions.

---
+++ tools/qa_hamsta/qa_hamsta/db/patches/201302261200_added_permissions.sql
@@ -0,0 +1,9 @@
+INSERT INTO privilege(privilege,descr) VALUES
+	('job_edit_custom','Edit custom job XMLs on the master'),
+	('job_edit_predefined','Edit predefined job XMLs on the master'),
+	('vm_admin','Create or delete virtual guests'),
+	('vm_startstop','Start or stop virtual guests'),
+	('vm_startstop_reserved','Start or stop virtual guests reserved by other user'),
+	('vh_admin','Create, delete or reinstall virtual hosts');
+INSERT INTO role_privilege(role_id,privilege_id) SELECT user_role.role_id,privilege.privilege_id FROM user_role,privilege WHERE user_role.role IN ('admin') AND privilege.privilege IN ('job_edit_predefined','vm_admin','vm_startstop_reserved','vh_admin');
+INSERT INTO role_privilege(role_id,privilege_id) SELECT user_role.role_id,privilege.privilege_id FROM user_role,privilege WHERE user_role.role IN ('admin','user') AND privilege.privilege IN ('job_edit_custom','vm_startstop');
