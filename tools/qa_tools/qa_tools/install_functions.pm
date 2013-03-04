687f57f449dc47974aa07835cde60a31926a2fca
jtang <jtang@suse.com>
2013-03-04 17:40:54 +0800

    revert code for disk name detect

--- tools/qa_tools/qa_tools/install_functions.pm
+++ tools/qa_tools/qa_tools/install_functions.pm
@@ -126,8 +126,6 @@ sub disk_stats
 			$dev="/dev/hd".$short;
 		}
 	}
-	### ignore above if segment, because sometimes disk judgement by "_has_libsata" is not correct, see bug #745785.
-	$dev=`df -l |grep dev |sed -n 1p | awk {'print \$1'} | sed 's/[0-9]\$//g'`;
 	chomp($dev);
 	return ($dev,$num);
 }
