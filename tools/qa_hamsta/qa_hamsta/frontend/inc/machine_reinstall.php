e64c81bf445b2a2c8d0210314ddf6939e0759eaf
Pavel Kacer <pkacer@suse.com>
2013-04-15 14:20:05 +0200

    Hamsta: Fixed reinstall permission name at the frontend.

--- tools/qa_hamsta/qa_hamsta/frontend/inc/machine_reinstall.php
+++ tools/qa_hamsta/qa_hamsta/frontend/inc/machine_reinstall.php
@@ -51,7 +51,7 @@ foreach($machines as $m) {
 
 /* Now check if the user tries to reinstall only his machines or if
  * he can reinstall also reserved machines. */
-$perm=array('owner'=>'machine_reinstall','other'=>'machine_reserved','url'=>'index.php?go=machine_reinstall');
+$perm=array('owner'=>'machine_reinstall','other'=>'machine_reinstall_reserved','url'=>'index.php?go=machine_reinstall');
 machine_permission_or_disabled($machines,$perm);
 
 # If the install options are empty, we use the ones from the DB, else we see if options are different between machines. If different, don't use them
