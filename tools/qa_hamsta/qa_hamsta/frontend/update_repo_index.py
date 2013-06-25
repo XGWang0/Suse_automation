2a088385c458004b4c63d2e302813e6a37a7e92c
Han Zhibin <zbhan@suse.com>
2013-06-25 10:57:54 +0800

    complete fate 315108 - Change year in licences to 2013

--- tools/qa_hamsta/qa_hamsta/frontend/update_repo_index.py
+++ tools/qa_hamsta/qa_hamsta/frontend/update_repo_index.py
@@ -1,1 +1,152 @@
-update-repo-index.py
+#!/usr/bin/python
+
+# ****************************************************************************
+# Copyright (c) 2013 Unpublished Work of SUSE. All Rights Reserved.
+# 
+# THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+# CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+# RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+# THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+# THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+# TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+# PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+# PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+# AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+# LIABILITY.
+# 
+# SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+# AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+# LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+# WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+# ****************************************************************************
+
+"""Generate network installation sources repo index and sdk index
+The index is written as 'default.repo.json' and 'default.sdk.json' under current directory by default.
+In case you don't have write access to your repo, you can run it remotely too
+by passing the url of your repo, we will fetch and parse.
+
+DirectoryStructure
+====================
+	product/arch/DVDx
+
+Usage
+=======
+	$ python update-repo-index.py -r "<repourl1> [repourl2] [...]" -s "<sdkurl1> [sdkurl2] [...]" -o <output_name>
+	repourl could be "http://dist.suse.de/install/SLP/"
+"""
+import os,re,urllib2,optparse,json,sys,gzip,StringIO
+supported_archs = ['i386', 'i586', 'x86_64', 'ia64', 'ppc', 'ppc64', 's390x']
+basename = 'default'
+
+def list_dir(url):
+	"""Parsing the web page and get the directory list, os.listdir clone"""
+	page = urllib2.urlopen(url).read()
+	return re.findall('<img.*?folder.(?:gif|png).*?href="(.*?)/">', page)
+
+def append_result(repo, product, arch, am):
+	p = {}
+	p['product'] = product
+	p['arch'] = arch
+	p['url'] = os.path.join(repo, product, am)
+	baserepo = p['url']
+	p['pattern'] = []
+
+	if (re.match('i.86$', arch)):
+		matcharch ='i.86'
+	elif (re.match('ppc', arch)):
+		matcharch = 'ppc(64)?'
+	else:
+		matcharch = arch
+	re_arch = re.compile('\.' + matcharch + '\.pat(\.gz)?$')
+	re_pat = re.compile('^=Pat: ')
+	try:
+		# patfiles contain list of .pat or .pat.gz files with pattern definitions
+		patfiles = filter(re_arch.search, urllib2.urlopen(baserepo + "/suse/setup/descr/patterns").read().split("\n"))
+		if not patfiles:
+			print "Pattern descriptor (" + baserepo + "/suse/setup/descr/patterns) is empty. No pattern info grepped."
+		for patfile in patfiles:
+			# Some patfile contain definition of multiple patterns, while some not
+			# Patterh name line looks like:
+			# =Pat:  Basis-Devel 11 89.17.4 i586
+			data = urllib2.urlopen(baserepo + "/suse/setup/descr/" + patfile).read()
+			if (patfile.endswith('.gz')):
+				data = gzip.GzipFile(fileobj = StringIO.StringIO(data)).read()
+			for line in filter(re_pat.search, data.split("\n")):
+				p['pattern'].append(line.split(' ')[2])
+	except:
+		print "Can not read " + baserepo + "/suse/setup/descr/patterns (either non-exsit or broken)"
+	print 'Found', product, arch, " on  ", repo
+	return p
+
+def generate_index(repo_url, theFilter):
+	"""Generate index for repo
+	repo_url is where the repo can be accessed publicly by http
+	"""
+	result = []
+	for repo in repo_url.split():
+		products = list_dir(repo)
+		newlist = []
+		for product in products:
+			for i in theFilter['+']:
+				if i in product.lower():
+					newlist.append(product)
+			for i in theFilter['-']:
+				if i in product.lower():
+					try:
+						newlist.remove(product)
+					except:
+						pass
+		for item in newlist:
+			dirs = list_dir(os.path.join(repo, item))
+			if 'suse' in dirs: #that means a plain mount, without ix86/DVD1. e.g. 147.2.207.242/iso_mnt
+				if 'DVD1' not in item and 'openSUSE' not in item and 'Media1' not in item:
+					continue
+				subdirs = list_dir(os.path.join(repo, item, 'suse'))
+				for arch in supported_archs:
+					if arch in item:
+						break
+				am = '' #am means arch+media part in the url string
+				result.append(append_result(repo, item, arch, am))
+			else:
+				for arch in dirs:
+					if arch not in supported_archs:
+						continue
+					subdirs = list_dir(os.path.join(repo, item, arch))
+					if 'media.1' in subdirs:
+						am = arch
+					elif 'DVD1' in subdirs:
+						am = arch + "/DVD1"
+					else:
+						am = arch + "/CD1"
+					result.append(append_result(repo, item, arch, am))
+	def product(s):
+		return s['product']
+	sresult = sorted(result, key=product)
+	return sresult
+
+# Main program entry
+if __name__ == '__main__':
+	parser = optparse.OptionParser()	
+	group = optparse.OptionGroup(parser, 'Example', './update-repo-index.py -r "http://147.2.207.242/iso_mnt/ http://147.2.207.208/dist/install/SLP/" -s "http://147.2.207.242/iso_mnt/ http://147.2.207.208/dist/install/" -o cn')
+	parser.add_option("-r", "--repo", dest="repourl", help = 'The base url(s) of your install repo, support multiple urls which sperated by space')
+	parser.add_option("-s", "--sdk", dest="sdkurl", help = 'The base url(s) of your sdk repo, support multiple urls which sperated by space')
+	parser.add_option("-o", "--output", dest="output", help = 'The output index file base name, default value is "default" if -o option is not given')
+	parser.add_option_group(group)
+	(options, args) = parser.parse_args()
+	if not (options.repourl or options.sdkurl):
+		print 'Install repo url and SDK repo url are required, please try ./update-repo-index.py -h'
+		sys.exit()
+	else:
+		if options.output:
+			basename = options.output
+		theFilter = {'+':["sles","sled","opensuse"], '-':["sdk"]}
+		json.dump(generate_index(options.repourl, theFilter),
+		open("%s.repo.json" %basename, 'w'), indent = 2)
+		theFilter = {'+':["sdk"], '-':["vmware"]}
+		if options.sdkurl:
+			json.dump(generate_index(options.sdkurl, theFilter),
+			open("%s.sdk.json" %basename, 'w'), indent = 2)
+
--- tools/qa_hamsta/qa_hamsta/master/master.pl
+++ tools/qa_hamsta/qa_hamsta/master/master.pl
@@ -1,1 +1,183 @@
-master_cmdline_server.pl
+#!/usr/bin/perl -w 
+# ****************************************************************************
+# Copyright (c) 2013 Unpublished Work of SUSE. All Rights Reserved.
+# 
+# THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+# CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+# RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+# THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+# THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+# TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+# PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+# PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+# AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+# LIABILITY.
+# 
+# SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+# AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+# LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+# WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+# ****************************************************************************
+#
+
+# This is the Master.
+#
+ 
+package Master;
+
+use warnings; 
+use strict;
+
+use IO::File;
+use File::Path; 
+use XML::Dumper;
+use Proc::Fork;
+
+BEGIN { push @INC, '.', '/usr/share/hamsta/master', '/usr/share/qa/lib'; }
+use log;
+require sql;
+use functions; 	
+use cmdline;
+use db_common;
+
+# this hash holds the whole information (thats why backbone) about the master and of the slaves
+our $backbone = { } ;
+
+# load the configuration 
+use qaconfig('%qaconf','&get_qa_config');
+%qaconf = ( %qaconf, &get_qa_config('hamsta_master') );
+
+
+# Master->deconstruct()
+#
+# this function should be used for deconstructing the HAMSTA
+# furthermore for bringing the system in a defined state
+#
+sub deconstruct() {
+    &log(LOG_NOTICE,"MASTER->DECONSTRUCT: exit-wish received, doing clean up");
+
+# TODO Close these links in the cmdline destructor
+#    foreach my $socket (@commandline_clients) {
+#        print "MASTER->DECONSTRUCT: destroy commandline user connection \n";
+#        eval {
+#            close ${$socket};
+#        };
+#        print "MASTER->DECONSTRUCT: WARNING: termination on socket: $@ \n" if ($@);
+#    }
+    
+    # empty the machine table, so we have to initialize and fill them later again
+    &sql_get_connection();
+
+    &TRANSACTION( 'machine','job_on_machine','job' );
+    &machine_set_all_unknown();
+    &TRANSACTION_END;
+
+    exit -1;	
+    # TODO: write log information into file or database, that master has gone
+}
+
+# Master->load_config()
+#
+# sets the configured values from qaconfig file
+# eg. root-directory, port for command line server, for slaves
+# important is, that it deletes the temporary files
+#
+sub load_config() {
+    if ($qaconf{'hamsta_master_root'}) {
+        $backbone->{'master_root'} = $qaconf{'hamsta_master_root'};
+    } else {
+        &log(LOG_WARNING,"No master_root set, defaulting to /tmp/master");
+        $backbone->{'master_root'} = "/tmp/master/";
+    }
+
+    if (!($qaconf{hamsta_master_cli_port}) || !($qaconf{hamsta_master_max_cli_connections})) {
+        $qaconf{hamsta_master_cli_port} = 18431;
+        $qaconf{hamsta_master_max_cli_connections} = 1024;
+    }	
+
+    if (!($qaconf{hamsta_client_port})) {
+        $qaconf{hamsta_client_port} = 2222;
+    }
+
+}
+
+#################################
+# the startup sequence
+#################################
+$SIG{'HUP'} = sub { die(); };
+$SIG{'CHLD'} = 'IGNORE';
+
+# configuration:
+# set a default layout of the variable backbone
+$backbone->{'active'} = { };
+
+
+# load configuration 
+&load_config();
+sleep 1;
+
+our $respawn_delay = $qaconf{'hamsta_master_respawn_delay'};
+
+$0 = $log::loginfo = 'master';
+$log::loglevel = $qaconf{hamsta_master_loglevel_main} if $qaconf{hamsta_master_loglevel_main};
+&log_add_output( path=>$qaconf{'hamsta_master_root'}.'/error.log', level=>LOG_ERROR );
+# Initialisation
+&log(LOG_INFO, "Starting multicast server");
+child {
+	# MULTICAST SERVER
+	use active_hosts;
+
+	$0 = $log::loginfo = 'active_hosts';
+	$log::loglevel = $qaconf{hamsta_master_loglevel_multicast} if $qaconf{hamsta_master_loglevel_multicast};
+	if( &sql_get_connection() )	{
+		while(1)	{
+			eval { &active_hosts(); };
+			&log(LOG_ERROR, "active_hosts died (restarting in $respawn_delay s): $@");
+			sleep $respawn_delay;
+		}
+	}
+	exit;
+}
+parent {
+    &log(LOG_DETAIL, "active_hosts is PID ".(shift));
+};
+
+&log(LOG_INFO, "Starting scheduler");
+child {
+	# SCHEDULER
+	use scheduler_ng;
+
+	$0 = $log::loginfo = 'scheduler';
+	$log::loglevel = $qaconf{hamsta_master_loglevel_scheduler} if $qaconf{hamsta_master_loglevel_scheduler};
+	&log_add_output( path=>$qaconf{'hamsta_master_root'}.'/scheduler.log' );
+	if( &sql_get_connection() )	{
+		while(1)	{
+			eval { &scheduler(); };
+			&log(LOG_ERROR, "scheduler died (restarting in $respawn_delay s): $@");
+			sleep $respawn_delay;
+		}
+	}
+	exit;
+}
+parent {
+	&log(LOG_DETAIL, "scheduler is PID ".(shift));
+};    
+
+# command-Interface 
+&log_add_output( path=>$qaconf{'hamsta_master_root'}.'/master.log' );
+
+
+# initialize the destroy functions
+$SIG{KILL} = \&deconstruct;
+$SIG{INT} = \&deconstruct;
+$SIG{TERM} = \&deconstruct;
+while(1) {
+	&log(LOG_INFO, "Starting command interface, Port: ".$qaconf{hamsta_master_cli_port});
+	eval { &command_line_server(); };
+	&log(LOG_WARNING, "Command line server died (restarting in $respawn_delay s): $@");
+	sleep $respawn_delay;
+}
+
