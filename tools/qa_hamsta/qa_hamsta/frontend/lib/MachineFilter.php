412431a8f3685f63afcdc57e51166d36ebe8dc85
Han Zhibin <zbhan@suse.com>
2013-07-26 15:26:49 +0800

    add new filter class for parse the given text

---
+++ tools/qa_hamsta/qa_hamsta/frontend/lib/MachineFilter.php
@@ -0,0 +1,83 @@
+<?php
+
+class MachineFilter
+{
+
+	private $machine_list = array ();
+	private $search_fields = array ();
+	private $match_fields= array ();
+	private $search_text = "";
+
+	public function __construct ($machines, $text, $search_fields = array ())
+	{
+		$this->machine_list = $machines;
+		$this->search_fields = $search_fields;
+		$this->search_text = $text;
+	}
+
+	public function setSearchFields ($fields_array)
+	{
+		$this->search_fields = $fields_array;
+	}
+
+	public function getSearchFields ()
+	{
+		return array_unique($this->search_fields);
+	}
+	
+	public function setMachines($machines)
+	{
+		$this->machine_list= $machines;
+	}
+
+	public function getMachines()
+	{
+		return $this->machine_list;
+	}
+
+	public function filter()
+	{
+		$tmp_machine_list = array();
+
+		if (!isset($this->machine_list) || !is_array($this->machine_list))
+			return;
+		if (!isset($this->search_fields) || !is_array($this->search_fields))
+			return;
+
+		foreach (array_keys($this->search_fields) as $field)
+		{
+			$fname = "get_" . $field;	
+			foreach ($this->machine_list as $machine)
+			{
+				if (! method_exists($machine, $fname))
+					continue;
+				$value = $machine->$fname();
+				//if (preg_match($this->search_text, $value) == 1)
+				if ($value && strstr($value, $this->search_text))
+				{
+					//if (!in_array($field, $this->search_fields))
+					//    $this->search_fields[] = $field;
+					if (!in_array($field, $this->match_fields))
+						$this->match_fields[] = $field;
+					
+					if (count($tmp_machine_list) == 0)
+						$tmp_machine_list[] = $machine;
+					else 
+					{
+						$last_machine = $tmp_machine_list[count($tmp_machine_list)-1];	
+						if ($last_machine->get_id() != $machine->get_id())
+							$tmp_machine_list[] = $machine;
+					}
+				}
+			}
+		}
+		return $tmp_machine_list;
+	}
+
+	public function getMatchFields ()
+	{
+		return $this->match_fields;
+	}
+	
+
+}
