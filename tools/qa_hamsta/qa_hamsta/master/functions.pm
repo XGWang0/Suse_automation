3b5290268d4b8d22e0f65043b84e48c233b0858f
Pavel Kacer <pkacer@suse.com>
2013-11-29 14:53:57 +0100

    Fix for the product parsing bug.
     * Fixes wrong regular expression that was unable to recognize SP
       product parts properly.
     * Makes the code more readable and easier for debugging, the regex
       was unreadable hard to debug.

--- tools/qa_hamsta/qa_hamsta/master/functions.pm
+++ tools/qa_hamsta/qa_hamsta/master/functions.pm
@@ -91,21 +91,49 @@ sub process_product($)
 	# expected $prod is:
 	#    3.0.58-0.9-default;SLES11(x86_64)VERSION=11PATCHLEVEL=3
 	#    3.7.1-1-desktop;openSUSE12.3Beta1(x86_64)VERSION=12.3CODENAME=DartmouthBeta1-Kernel\r
-        if( $prod =~ /^([\w\.\-\+_]+);(SLES4SAP|openSUSE|[[:alpha:]]+)([\dSP\.]+)?(\w*)?(\(([\w\-]+)\))?VERSION=/ ) 
-	{
-		my ($base,$major,$sp,$rel,$dom,$build,$arch)=($2,$3,$4,'','','','');
-		$sp 	="SP$1" if $prod =~ /PATCHLEVEL=(\d+)/;
-		$major	= $1 if $major =~ /(\d+)SP/;
-		$arch	= $1 if $prod  =~ /\(([^\)]+)\)/;
-		$rel	= $1 if $prod  =~ /(Alpha\d+|Beta\d+|GMC?|Build\d+|RC\d+|Internal|Maintained)/i;
-		$dom	= "xen$1" if $prod  =~ /Dom([A-Z\d]+)/;
-		$build	= $1 if $prod  =~ /(Build\d+)/;
-		my $product = join '-', grep /\w/, ($base,$major,$sp);
-		my $release = join '-', grep /\w/, ($rel,$build);
-		my $p_arch  = join '-', grep /\w/, ($dom,$arch);
-#		return [$base,$major,$sp,$rel,$dom,$build,$arch];
-		return [$product,$release,$p_arch];
+
+	my ($kernel, $product, $arch, $rest) = ('', '', '', '');
+	# Chop the whole product line into categorical pieces
+	#
+	# kernel contains everything before ';'
+	# product contains the product (between the ';' and the left of the opening '(')
+	# arch contains the architecture in '(...)' without the parenthesis
+	# rest contains the rest of the line
+	$kernel = $1 if $prod =~ /^([\w.-]+);/;
+	$product = $1 if $prod =~ /;([\w\d.]+)/;
+
+	# Gather architecture and the rest of the line,
+	# e.g. from (x86_64)VERSION=11PATCHLEVEL=3
+	if ($prod =~ /\(([\w_]+)\)([\w=.+-]+)$/) {
+	    $arch = $1;
+	    $rest = $2
 	}
+
+	if ($kernel and $product and $arch) {
+	    # Initialize target hash and use some gathered values
+	    my %parts = (
+		kernel	=> $kernel,
+		major	=> '',
+		sp	=> '',
+		rel	=> '',
+		dom	=> '',
+		build	=> '',
+		arch	=> $arch,
+	    );
+
+	    # Assign specific values
+	    $parts{'major'}	= $1 if $product =~ /(SLES4SAP|[[:alpha:]]+)/;
+	    $parts{'sp'}	= $1 if $product =~ /(SP\d+)/;
+	    $parts{'rel'}	= $1 if $product  =~ /(Alpha\d+|Beta\d+|GMC?|Build\d+|RC\d+|Internal|Maintained)/i;
+	    $parts{'dom'}	= "xen$1" if $product =~ /Dom([A-Z\d]+)/;
+	    $parts{'build'}	= $1 if $product  =~ /(Build\d+)/;
+
+	    my $product = join '-', grep /\w/, ($parts{'kernel'},$parts{'major'},$parts{'sp'});
+	    my $release = join '-', grep /\w/, ($parts{'rel'},$parts{'build'});
+	    my $p_arch  = join '-', grep /\w/, ($parts{'dom'},$parts{'arch'});
+	    return [$product,$release,$p_arch];
+	}
+
 	return [$prod,'',''];
 }
 
