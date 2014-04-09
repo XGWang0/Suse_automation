3c221c44632f77d3e3465950648f464f1e01ec97
alice <xlai@suse.com>
2014-04-09 11:13:12 +0800

    Rename fields in table hamsta_master.

--- tools/qa_hamsta/qa_hamsta/db/patches/201403281350_machine_hamsta_master.sql
+++ tools/qa_hamsta/qa_hamsta/db/patches/201403281350_machine_hamsta_master.sql
@@ -1,9 +1,10 @@
 CREATE TABLE hamsta_master (
-  master_id int not null auto_increment primary key,
-  master_ip varchar(16) not null,
-  unique(master_ip)
+  hamsta_master_id int not null auto_increment primary key,
+  hamsta_master varchar(255) not null,
+  hamsta_master_ip varchar(16) not null,
+  unique(hamsta_master_ip)
 ) ENGINE=InnoDB; 
 
 ALTER TABLE `machine` ADD COLUMN hamsta_master_id int after `machine_status_id`;
-ALTER TABLE `machine` ADD CONSTRAINT `fk_machine_hamsta_master_id` FOREIGN KEY(`hamsta_master_id`) REFERENCES `hamsta_master`(`master_id`) ON DELETE RESTRICT;
+ALTER TABLE `machine` ADD CONSTRAINT `fk_machine_hamsta_master_id` FOREIGN KEY(`hamsta_master_id`) REFERENCES `hamsta_master`(`hamsta_master_id`) ON DELETE RESTRICT;
 
