b2f386053ac3a4df9b4f139e55ff431a5eb60cc7
alice <xlai@suse.com>
2014-04-18 10:00:42 +0800

    Rename fields and fk.

--- tools/qa_hamsta/qa_hamsta/db/patches/201403281350_machine_hamsta_master.sql
+++ tools/qa_hamsta/qa_hamsta/db/patches/201403281350_machine_hamsta_master.sql
@@ -1,10 +1,10 @@
 CREATE TABLE hamsta_master (
   hamsta_master_id int not null auto_increment primary key,
-  hamsta_master_name varchar(255) not null,
+  hamsta_master varchar(255) not null,
   hamsta_master_ip varchar(16) not null,
   unique(hamsta_master_ip)
 ) ENGINE=InnoDB; 
 
 ALTER TABLE `machine` ADD COLUMN hamsta_master_id int after `machine_status_id`;
-ALTER TABLE `machine` ADD CONSTRAINT `fk_machine_hamsta_master_id` FOREIGN KEY(`hamsta_master_id`) REFERENCES `hamsta_master`(`hamsta_master_id`) ON DELETE RESTRICT;
+ALTER TABLE `machine` ADD CONSTRAINT `fk_machine_hamsta_master` FOREIGN KEY(`hamsta_master_id`) REFERENCES `hamsta_master`(`hamsta_master_id`) ON DELETE RESTRICT;
 
