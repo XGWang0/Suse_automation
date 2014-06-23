021151b40fd20f04f8b21bd4de34331010434e5b
Pavel Kacer <pkacer@suse.com>
2014-06-23 13:48:46 +0200

    Minor code formatting changes and command pipes reduction.

--- tools/qa_tools/qa_tools/addsut.pl
+++ tools/qa_tools/qa_tools/addsut.pl
@@ -29,15 +29,16 @@ if ($ret != 0) { #SLE
 	}
 } else {
 	$repo = "openSUSE-";
-	$mycmd = "cat /etc/SuSE-release | grep -i VERSION | sed -e \'s/[A-Za-z =]//g\'";
+	$mycmd = "grep -i VERSION /etc/SuSE-release | sed -e \'s/[A-Za-z =]//g\'";
 	$OSVer = readpipe($mycmd);
 	chomp($OSVer);
 	if ( $OSVer == "" ) {
-               print "Can not get $repo OS version!";
-               exit 1;
-        }
+		print "Can not get $repo OS version!";
+		exit 1;
+	}
 	$repo .= $OSVer;
 }
+
 my $repo_url = $pre_repo . "/" . $repo . "/";
 $mycmd = "zypper lr -u|grep $repo_url|awk -F\\\| \'{print \$4}\'|grep \"Yes\" 1>/dev/null";
 $repoOK = system($mycmd);
@@ -49,18 +50,21 @@ if ($repoOK != 0) {
 		exit 1;
 	}
 }
+
 $mycmd = "zypper --no-gpg-checks --gpg-auto-import-keys in -y qa_hamsta 1>/dev/null";
 $ret = system($mycmd);
 if ($ret != 0) {
 	print "qa_hamsta cannot be added to SUT.";
 	exit 1;
 }
+
 $mycmd = "/usr/share/qa/tools/get_net_addr.pl";
 $sut_net_addr = `$mycmd`;
 if ( $sut_net_addr == "" ) {
 	print "Can not get sut_net_addr.";
 	exit 1;
 }
+
 if ( $sut_net_addr ne $master_net ) {
 	$mycmd = "sed -i s/hamsta_multicast_address=\\\'.*\\\'/hamsta_multicast_address=\\\'$master_ip\\\'/ /etc/qa/00-hamsta-common-default";
 	system($mycmd);
@@ -71,6 +75,7 @@ if ( $sut_net_addr ne $master_net ) {
 		exit 1;
 	}
 }
+
 $mycmd = "rchamsta start >/dev/null";
 $ret = system($mycmd);
 if ($ret != 0) {
