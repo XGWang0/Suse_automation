927580ecfe8e384937ac85b2e889f740b326f19e
Vilem Marsik <vmarsik@suse.cz>
2014-11-19 16:48:28 +0100

    Fixed SLE-12 update, after testing with yac

--- tools/qa_qadb/qa_qadb/db/patches/20141009_add_maint2_format.sql
+++ tools/qa_qadb/qa_qadb/db/patches/20141009_add_maint2_format.sql
@@ -1,6 +1,5 @@
 -- Maintenance requests patches identified as issuer_id:issue_id:request_id, in addition to existing md5sum
 ALTER TABLE submission 
 	ADD COLUMN issuer_id ENUM('SUSE','openSUSE'),
-	ADD COLUMN issue_id INT NULL,
-	CHANGE `type` `type` ENUM('prod','kotd','maint','maint2');
+	ADD COLUMN issue_id INT NULL;
 UPDATE submission SET issuer_id='SUSE' WHERE type='maint';
